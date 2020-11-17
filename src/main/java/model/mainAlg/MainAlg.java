227b488ab699ce69311b275f4794c1cdd6651239
Alexey Dubynin <alexeyka791@gmail.com>
2020-11-17 19:29:30 +0300

    A* added

--- src/main/java/model/mainAlg/MainAlg.java
+++ src/main/java/model/mainAlg/MainAlg.java
@@ -1,13 +1,45 @@
 package model.mainAlg;
 
-import model.parser.Parser;
+
+import model.object.Map;
+import model.object.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainAlg {
+
+    private StringBuilder ans = new StringBuilder("");
+    static Map map = new Map();
+
     public static void main(String[] args) {
-        Parser parser = new Parser("src/main/resources/example-01.desc");
-        System.out.println(parser.parseSizeMap());
-        System.out.println(parser.parseStartPositionRobot());
-        System.out.println(parser.parseWallPosition());
-        System.out.println(parser.parseBoosters());
+
+        List<Pair<Integer, Integer>> mapSize = new ArrayList<>();
+        mapSize.add(new Pair<>(0, 0));
+        mapSize.add(new Pair<>(7, 0));
+        mapSize.add(new Pair<>(7, 5));
+        mapSize.add(new Pair<>(0, 5));
+        map.createMap(mapSize);
+
+        List<Pair<Integer, Integer>> obstacles = new ArrayList<>();
+        obstacles.add(new Pair<>(3, 2));
+        obstacles.add(new Pair<>(4, 2));
+        obstacles.add(new Pair<>(4, 4));
+        obstacles.add(new Pair<>(3, 4));
+
+        obstacles.add(new Pair<>(2, 3));
+        obstacles.add(new Pair<>(3, 3));
+        obstacles.add(new Pair<>(3, 4));
+        obstacles.add(new Pair<>(2, 4));
+        map.createObstacles(obstacles);
+
+        System.out.println(map.getX() + " " + map.getY());
+
+        AStar star = new AStar(map);
+        System.out.println(star.GetPath(1, 2, 5, 3));
     }
+
+
+
+
 }
